07a49b0803a1c04a504d3d0f6a48cada1169b759
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2023-09-01 12:10:09 +0530

    commit UseMemo

---
+++ React_Functionality/Sec_Project_usememo/src/components/Demo/DemoList.js
@@ -0,0 +1,27 @@
+import React, { useMemo } from "react";
+
+import classes from "./DemoList.module.css";
+
+const DemoList = (props) => {
+  const { items } = props;
+
+  const sortedList = useMemo(() => {
+    console.log("items sorted");
+    return items.sort((a, b) => a - b);
+  }, [items]);
+
+  console.log("Demo running");
+
+  return (
+    <div className={classes.list}>
+      <h2>{props.title}</h2>
+      <ul>
+        {sortedList.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default React.memo(DemoList);
